f4a7d0876e36c29f0e12a9ff2752e00b7cc69ac0
Alejandro Lopez <alejandrolopezgomez2004@gmail.com>
2026-05-24 03:50:27 +0200

    Modificacion: se añade nueva funcionalidad al fichero PHP

--- index.php
+++ index.php
@@ -12,4 +12,5 @@ $mensaje = "Hola Mundo!";
 // Mostramos el mensaje por pantalla
 echo "<h1>" . $mensaje . "</h1>";
 echo "<p>Este es mi primer proyecto PHP con control de versiones Git.</p>";
+echo "<p>Version 2: linea anadida para demostrar el control de versiones.</p>";
 ?>
