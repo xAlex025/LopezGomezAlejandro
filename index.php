5ffdc3ff825af6e4fe16ee26f24f892dabf1b052
Alejandro Lopez <alejandrolopezgomez2004@gmail.com>
2026-05-24 03:39:28 +0200

    Primer commit: proyecto PHP inicial

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Pagina principal del proyecto
+ *
+ * @author Alejandro Lopez Gomez
+ * @version 1.0
+ */
+
+// Definimos el mensaje de bienvenida
+$mensaje = "Hola Mundo!";
+
+// Mostramos el mensaje por pantalla
+echo "<h1>" . $mensaje . "</h1>";
+echo "<p>Este es mi primer proyecto PHP con control de versiones Git.</p>";
+?>
